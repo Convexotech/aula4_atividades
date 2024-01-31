376c58c4ed3172cc30202abf89672e198f58ff9c
Convexotech <157659610+Convexotech@users.noreply.github.com>
2024-01-31 19:34:14 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+#CORREÇÃO AULA 4:  
+
+# 1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
+
+print(10)
+
+
+# 2 -
+# Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+
+name   =  'Beatriz'
+surname =  'Alves'
+print(name+' ', surname)
+
+
+
+# 3 -
+# Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+n1 = input('Digite o primeiro número: ')
+n2 = input('Digite o segundo número: ')
+print(n1,n2)
+
+# 4 -
+# Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+
+word  = 'Python'
+print(word, 10)
+
+
+# 5 -
+# Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado.
+
+frase =  'Olá Mundo!'
+user_word =  input('Digite uma palavra: ')
+
+print(frase, user_word )
+
